8f081391306d20d26344b1931d33dc31d28b9d71
Pratik Gupte <pratikgupte16@gmail.com>
2025-01-10 12:26:02 +0000

    WIP Add basic tests; failing

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,25 +1,116 @@
-# TODO: move setup to separate file
-gbr <- daedalus::daedalus_country("GBR")
-infection <- daedalus::daedalus_infection("influenza_2009")
+# test basic scenario runners
+test_that("Generating multiple infection samples", {
+  expect_no_condition(
+    make_infection_samples("influenza_1918")
+  )
+  checkmate::expect_list(
+    make_infection_samples("influenza_1918"),
+    "daedalus_infection"
+  )
+})
 
-low_severity <- daedalus::get_data(daedalus_infection(""))
-high_severity <- daedalus::get_data(daedalus_infection("sars_cov_1"), "gamma_H")
+test_that("Running multiple infection and response scenarios", {
+  infection_list <- make_infection_samples("influenza_2009", samples = 10)
 
-r0_samples <- round(withr::with_seed(1, rnorm(100, r0, 0.2)), 2)
-r0_interval <- ci(r0_samples)
-r0_limits <- c(
-  lower = r0 - r0_interval,
-  upper = r0 + r0_interval
-)
+  expect_no_condition(
+    run_scenarios(
+      "GBR", infection_list,
+      response = "none"
+    )
+  )
+  # expect output is a data table with list columns
+  output <- run_scenarios(
+    "GBR", infection_list,
+    response = "none"
+  )
+  expect_data_table(
+    output
+  )
+  checkmate::expect_list(output$output, "list")
 
-# estimate final sizes in an unmitigated pandemic
-lapply(r0_limits, finalsize::final_size)
+  # expect running scenarios works for multiple pre-defined responses
+  responses <- c("none", "elimination")
+  expect_no_condition(
+    run_scenarios(
+      "GBR", infection_list,
+      response = responses
+    )
+  )
+  output <- run_scenarios(
+    "GBR", infection_list,
+    response = responses
+  )
+  expect_data_table(
+    output,
+    nrows = length(responses)
+  )
+  checkmate::expect_list(output$output, "list")
+})
 
-disease_x <- lapply(
-  r0_limits, function(x) {
-    daedalus_infection("influenza_2009", r0 = x)
-  }
-)
+test_that("Running custom response scenarios", {
+  # single custom response
+  response <- seq(0.5, daedalus:::N_ECON_SECTORS)
+  expect_no_condition(
+    run_scenarios(
+      "GBR", "influenza_2009", list(custom = response)
+    )
+  )
+  output <- run_scenarios(
+    "GBR", "influenza_2009", list(custom = response)
+  )
+  expect_data_table(
+    output
+  )
+  expect_list(
+    output$output, "daedalus_output"
+  )
 
-disease_tags <- c("lower", "upper")
-names(disease_x) <- disease_tags
+  # multiple custom responses
+  expect_no_condition(
+    run_scenarios(
+      "GBR", "influenza_2009",
+      list(custom = response, custom2 = response)
+    )
+  )
+  output <- run_scenarios(
+    "GBR", "influenza_2009", list(response, response)
+  )
+  expect_data_table(
+    output
+  )
+  expect_list(
+    output$output, "daedalus_output"
+  )
+
+  # custom responses and multiple infections
+  infection_list <- make_infection_samples("influenza_2009", samples = 10)
+  expect_no_condition(
+    run_scenarios(
+      "GBR", infection_list,
+      list(custom = response, custom2 = response)
+    )
+  )
+
+  # custom and pre-defined responses
+  # TODO: examine how names and openness coefficients are returned
+  expect_no_condition(
+    run_scenarios(
+      "GBR", "influenza_2009", list(response, "elimination")
+    )
+  )
+})
+
+test_that("Get epi curve data", {
+  infection_list <- make_infection_samples("influenza_2009", samples = 10)
+  response <- seq(0.5, daedalus:::N_ECON_SECTORS)
+
+  output <- run_scenarios(
+    "GBR", infection_list,
+    list(custom = response, custom2 = response)
+  )
+
+  # TODO: NOT WORKING
+  expect_no_condition(
+    get_epicurve_data(output)
+  )
+})
